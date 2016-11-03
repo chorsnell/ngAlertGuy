d0ae72e3f05c5dc902336b5a0a4b9e90b5ba884a
Chris <hel_mort@hotmail.com>
2016-11-03 16:18:31 +0000

    New functionality
    * Turned into service / got rid of rootscope
    * Specify content of alert via service
    * Attach callback to buttons
    * Customise button text
    * Made overlay properly clickable to close

--- ngAlertGuy.js
+++ ngAlertGuy.js
@@ -4,38 +4,66 @@
 @toc
 
 todo
-make work without rootscope, dat nasty
 make modal a template
-specify contents of alert, eg title and text
 types, eg error, success, info, etc
-attach callbacks to buttons
-customise button text
-make overlay clickable to close
 make buttons goto a particular path? eg back to start of vehicle section
-
 */
 
 'use strict';
 
 angular.module('ngAlertGuy', [])
-.factory('alertGuy', [ '$rootScope', function ( $rootScope ) {
+	.service('alertGuy', [function () {
 
-	//public methods
-	var self = {
+		//public methods
+		var self = {
+			alert: function (opts) {
+				self.toggle();
+				self.title = opts.title;
+				self.text = opts.text;
+				// optional
+				if (opts.confirmCallback) {
+					self.confirmCallback = opts.confirmCallback;
+				}
+				if (opts.dismissCallback) {
+					self.dismissCallback = opts.dismissCallback;
+				}
+			},
+			show: false,
+			toggle: function () {
+				self.show = !self.show;
+			},
+			title: '',
+			text: '',
 
-		alert: function (opts) {
-			$rootScope.alertGuy.toggle();
-			$rootScope.alertGuy.opts = opts;
-		}
-	};
+			confirm: function() {
+				self.toggle();
+				self.confirmCallback();
+			},
+			confirmButton: true,
+			confirmText: 'Ok',
+			confirmCallback: function() {
+				console.log('default confirm');
+			},
 
-	return self;
-}])
+			dismiss: function() {
+				self.toggle();
+				self.dismissCallback();
+			},
+			dismissButton: false,
+			dismissText: 'Cancel',
+			dismissCallback: function() {
+				console.log('default dismiss');
+			}
+		};
+
+		return self;
+	}])
 .directive('alertGuy', ['alertGuy', function (alertGuy) {
         return {
         	restrict: 'E',
-       		template: '<section class="section-modal modal-alert" ng-show="alertGuy.show" ng-click="alertGuy.toggle()">' +
-	'<div class="sub-overlay"><!-- --></div>' +
+       		template:
+'<section class="section-modal modal-alert" ng-show="alertGuy.show">' +
+	'<div class="sub-overlay" ng-click="alertGuy.toggle()"><!-- --></div>' +
 	'<div class="sub-inner mod-mid">' +
 		'<header class="header-modal">' +
 			'<div class="sub-controls">' +
@@ -43,19 +71,17 @@ angular.module('ngAlertGuy', [])
 			'</div>' +
 		'</header>' +
 		'<div class="container mod-mid">' +
-			'<h1>{{alertGuy.opts.title}}</h1>' +
-			'<div>{{alertGuy.opts.text}}</div>' +
-			'<button class="btn btn-primary btn-text btn-inline">OK</button>' +
+			'<h1>{{alertGuy.title}}</h1>' +
+			'<div>{{alertGuy.text}}</div>' +
+			'<br>' +
+			'<button class="btn btn-primary btn-text btn-inline" ng-show="alertGuy.confirmButton" ng-click="alertGuy.confirm()">{{alertGuy.confirmText}}</button>' +
+			'<button class="btn btn-primary btn-text btn-inline" ng-show="alertGuy.dismissButton" ng-click="alertGuy.dismiss()">{{alertGuy.dismissText}}</button>' +
 		'</div>' +
 	'</div>' +
 '</section>',
             link: function ($scope, elem, attrs) {
 
-				$scope.alertGuy = {};
-		    	$scope.alertGuy.show = false;
-		    	$scope.alertGuy.toggle = function() {
-		    		$scope.alertGuy.show = !$scope.alertGuy.show;
-		    	}
+				$scope.alertGuy = alertGuy;
             }
         };
     }]);
