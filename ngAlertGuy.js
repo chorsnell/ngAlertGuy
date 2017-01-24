948d0de19848b7545a64647c64fefa1c683a17da
Michael Holmes <michael@melonwebdesign.co.uk>
2017-01-24 16:12:22 +0000

    Add HTML capability and alerts as promises

--- ngAlertGuy.js
+++ ngAlertGuy.js
@@ -11,8 +11,8 @@ make buttons goto a particular path? eg back to start of vehicle section
 
 'use strict';
 
-angular.module('ngAlertGuy', [])
-	.service('alertGuy', [function () {
+angular.module('ngAlertGuy', ['ngSanitize'])
+	.service('alertGuy', ['$q', function ($q) {
 
 		//public methods
 		var self = {
@@ -20,6 +20,7 @@ angular.module('ngAlertGuy', [])
 				self.toggle();
 				self.title = opts.title;
 				self.text = opts.text;
+				self.allowHtmlText = !!opts.allowHtmlText;
 				// optional
 				if (opts.confirmCallback) {
 					self.confirmCallback = opts.confirmCallback;
@@ -28,6 +29,21 @@ angular.module('ngAlertGuy', [])
 					self.dismissCallback = opts.dismissCallback;
 				}
 			},
+
+			alertPromise: function (opts) {
+				return $q(function(resolve, reject) {
+					opts.confirmCallback = function() {
+						resolve(self);
+					};
+
+					opts.dismissCallback = function() {
+						reject('modalDismissed');
+					};
+
+					self.alert(opts);
+				});
+			},
+
 			show: false,
 			toggle: function () {
 				self.show = !self.show;
@@ -58,7 +74,9 @@ angular.module('ngAlertGuy', [])
 
 		return self;
 	}])
-.directive('alertGuy', ['alertGuy', function (alertGuy) {
+.directive('alertGuy', ['alertGuy', '$sce', function (alertGuy, $sce) {
+	var text = alertGuy.allowHtmlText? '<div ng-bind-html="alertGuy.text"></div>' : '<div>{{alertGuy.text}}</div>';
+	alertGuy.text = alertGuy.allowHtmlText? $sce.trustAsHtml(alertGuy.text) : alertGuy.text;
         return {
         	restrict: 'E',
        		template:
@@ -67,12 +85,12 @@ angular.module('ngAlertGuy', [])
 	'<div class="sub-inner mod-mid">' +
 		'<header class="header-modal">' +
 			'<div class="sub-controls">' +
-				'<button class="btn btn-med btn-inline btn-back mod-light icon-close" ng-click="alertGuy.toggle()"></button>' +
+				'<button class="btn btn-med btn-inline btn-back mod-light icon-close" ng-click="alertGuy.dismiss()"></button>' +
 			'</div>' +
 		'</header>' +
 		'<div class="container mod-mid">' +
 			'<h1>{{alertGuy.title}}</h1>' +
-			'<div>{{alertGuy.text}}</div>' +
+			text +
 			'<br>' +
 			'<button class="btn btn-primary btn-text btn-inline" ng-show="alertGuy.confirmButton" ng-click="alertGuy.confirm()">{{alertGuy.confirmText}}</button>' +
 			'<button class="btn btn-primary btn-text btn-inline" ng-show="alertGuy.dismissButton" ng-click="alertGuy.dismiss()">{{alertGuy.dismissText}}</button>' +
